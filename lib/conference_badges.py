bebe6c06d6a64a4eb4dfbb25c5cab62aa68834d8
Dovid Yarmush <yarmushda@gmail.com>
2023-06-13 11:03:08 -0400

    copleted badges and schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,31 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    names_list = []
+
+    for name in names:
+        names_list.append(f'Hello, my name is {name}.')
+
+    return names_list
 
 def assign_rooms(names):
-    return None
+    x = 1
+    room_list = []
+
+    for name in names:
+        room_list.append(f"Hello, {name}! You'll be assigned to room {x}!")
+        x += 1
+
+    return room_list
 
 def printer(names):
+    rooms_list = assign_rooms(names)
+    
+    for name in names:
+       print(badge_maker(name))
+    
+    for room in rooms_list:
+        print(room)
+
     return None
